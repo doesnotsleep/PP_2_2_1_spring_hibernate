46d9ee878aef6e9633b89b5550580ffb351355ca
doesnotsleep <asaff@mail.ru>
2023-03-05 01:29:14 +0300

    Work7

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImp.java
@@ -1,6 +1,7 @@
 package hiber.service;
 
 import hiber.dao.CarDao;
+import hiber.dao.UserDao;
 import hiber.model.Car;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,12 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 @Service
 public class CarServiceImp implements CarService{
-    @Autowired
+
    private CarDao carDao;
+    @Autowired
+    public CarServiceImp(CarDao carDao) {
+        this.carDao = carDao;
+    }
     @Transactional
     @Override
     public void add(Car car) {
